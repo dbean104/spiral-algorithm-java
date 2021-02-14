68b0a84531e4d667c2d833961aafae0aff4b7e9d
david <david@davids-mac-mini>
2021-02-14 16:20:53 +0000

    Initial commit of project structure

---
+++ src/main/java/com/dbean104/spiral/SpiralAlgorithm.java
@@ -0,0 +1,11 @@
+package com.dbean104.spiral;
+
+/**
+ * Main interface for calling the the spiral algorithm
+ * @author david
+ *
+ */
+public interface SpiralAlgorithm {
+	// TODO : Add methods
+
+}
